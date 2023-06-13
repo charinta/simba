fdd51d130aa1c382bf64704f6715d5ef0c7abf42
Nursina Hamdalah <nursinahamdalah21@gmail.com>
2023-06-14 03:18:24 +0700

    ngecilin login

--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -1,62 +1,52 @@
-@extends('layouts.user_type.guest')
+@extends('layouts.user_type.auth')
 
 @section('content')
-
-  <main class="main-content  mt-0">
+<main class="main-content mt-0">
     <section>
-      <div class="page-header min-vh-75">
-        <div class="container my-5">
-          <div class="row">
-     
-              <div class="card card-plain mt-8">
-                <div class="card-header pb-0 text-left bg-transparent">
-
-            
-      
-                </div>
-                <div class="card card-body" style="margin: 0rem;" class="col-md-4">
-                <h1 align="center" >Login</h1>
-                  <form role="form" method="POST" action="/session">
-                    @csrf
-                    <label class="fs-6">Email</label>
-                    <div class="mb-3">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="admin@softui.com" aria-label="Email" aria-describedby="email-addon">
-                      @error('email')
-                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
-                      @enderror
+        <div class="page-header min-vh-75">
+            <div class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <div class="card card-plain mt-8">
+                            <div class="card-header pb-0 text-left bg-transparent">
+                                <h1 class="text-center">Login</h1>
+                            </div>
+                            <div class="card-body">
+                                <form role="form" method="POST" action="/session">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label fs-6">Email</label>
+                                        <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="admin@softui.com" aria-label="Email" aria-describedby="email-addon">
+                                        @error('email')
+                                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="password" class="form-label fs-6">Password</label>
+                                        <input type="password" class="form-control" name="password" id="password" placeholder="Password" value="secret" aria-label="Password" aria-describedby="password-addon">
+                                        @error('password')
+                                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Login</button>
+                                    </div>
+                                    <div class="text-center">
+                                        <small class="text-muted">Forgot your password? Reset your password
+                                            <a href="/login/forgot-password" class="text-info text-gradient font-weight-bold">here</a>
+                                        </small>
+                                        <p class="mb-4 text-sm mx-auto">
+                                            Don't have an account?
+                                            <a href="register" class="text-info text-gradient font-weight-bold">Sign up</a>
+                                        </p>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <label class="fs-6">Password</label>
-                    <div class="mb-3">
-                      <input type="password" class="form-control" name="password" id="password" placeholder="Password" value="secret" aria-label="Password" aria-describedby="password-addon">
-                      @error('password')
-                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
-                      @enderror
-                    </div>
-
-                    <div class="text-center">
-                      <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Login</button>
-                    </div>
-                <div class="text-center">
-                <small class="text-muted">Forgot you password? Reset you password 
-                  <a href="/login/forgot-password" class="text-info text-gradient font-weight-bold">here</a>
-                </small>
-                  <p class="mb-4 text-sm mx-auto">
-                    Don't have an account?
-                    <a href="register" class="text-info text-gradient font-weight-bold">Sign up</a>
-                  </p>
                 </div>
-                </div>
-              </div>
             </div>
-            <div class="col-md-6">
-              <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
-            </form>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </section>
-  </main>
-
+</main>
 @endsection
