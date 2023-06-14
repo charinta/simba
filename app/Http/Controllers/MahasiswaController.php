e913085dafef7eb171551c187194c7c799e2913c
adzkykhairany <102246924+adzkykhairany@users.noreply.github.com>
2023-06-14 19:25:14 +0700

    tambah fungsi create di mahasiswa controller

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -7,8 +7,15 @@ use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
-     public function index(){
+    //fungsi untuk melihat tabel
+    public function index(){
             $mahasiswa   = Mahasiswa::all();
             return view('tables')->with('mahasiswa', $mahasiswa);
         }
+    
+    //fungsi untuk create data pada tabel
+    public function create(){
+        return view('create-mahasiswa');
+        }
+
 }
