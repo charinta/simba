6b308976c9e1766fa25e165e146dd8c865a7550f
charinta <charintakantidewi@gmail.com>
2023-06-15 18:57:01 +0700

    coba nambah p

--- resources/views/layouts/navbars/auth/nav-guest.blade.php
+++ resources/views/layouts/navbars/auth/nav-guest.blade.php
@@ -1,13 +1,18 @@
 <div class="container">
-  <nav class="navbar position top-0 z-index-3 my-3 {{ (Request::is('static-sign-up') ? 'w-100 shadow-none navbar-transparent mt-4' : 'blur blur-rounded shadow py-2 start-0 end-0 mx4') }}">
-    <div class="container-fluid d-flex align-items-center justify-content-between {{ (Request::is('static-sign-up') ? 'container' : 'container-fluid') }}">
-      <a style="color:DodgerBlue;" class="navbar-brand font-weight-bolder ms-lg-0 ms-3 {{ (Request::is('static-sign-up') ? 'text-white' : '') }}" href="{{ url('dashboard') }}">
-        SIMBA
-      </a>
-      <div class="d-flex">
-        <a class="btn btn-outline-primary text-primary me-3" href="{{ url('/login') }}">Login</a>
-        <a class="btn btn-primary" href="{{ url('register') }}">Register</a>
-      </div>
-    </div>
-  </nav>
+    <nav
+        class="navbar position top-0 z-index-3 my-3 {{ Request::is('static-sign-up') ? 'w-100 shadow-none navbar-transparent mt-4' : 'blur blur-rounded shadow py-2 start-0 end-0 mx4' }}">
+        <div
+            class="container-fluid d-flex align-items-center justify-content-between {{ Request::is('static-sign-up') ? 'container' : 'container-fluid' }}">
+            <a style="color:DodgerBlue;"
+                class="navbar-brand font-weight-bolder ms-lg-0 ms-3 {{ Request::is('static-sign-up') ? 'text-white' : '' }}"
+                href="{{ url('dashboard') }}">
+                SIMBA
+            </a>
+            <p>Halo</p>
+            <div class="d-flex">
+                <a class="btn btn-outline-primary text-primary me-3" href="{{ url('/login') }}">Login</a>
+                <a class="btn btn-primary" href="{{ url('register') }}">Register</a>
+            </div>
+        </div>
+    </nav>
 </div>
